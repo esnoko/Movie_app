e105cb526cb1c209b61f207c2a65b2e288f6b5ab
esnoko <elvisnoko18@gmail.com>
2025-02-28 16:16:11 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -47,7 +47,9 @@ const App = () => {
           ))}
         </div>
       ) : (
-       
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
       )}
     </div>
   );
