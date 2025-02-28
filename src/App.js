15ee301eda14d16da37bfba8a04e2e6110692b77
esnoko <elvisnoko18@gmail.com>
2025-02-28 17:09:24 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=463cf81c";
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
